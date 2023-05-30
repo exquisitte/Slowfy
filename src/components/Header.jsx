ab9c2995422d97bafdaeebcbd316ba4edc6498de
howtly <timofymatesha2@gmail.com>
2023-05-30 13:33:18 +0300

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Home from '../assets/icons8-главная-100 (4) 2.png'
+
+function Header() {
+
+    return (
+        <>
+          <div className="Header"> 
+            <img src={Home}></img>
+          </div>
+        </>
+      )
+    }
+    
+    export default Header
